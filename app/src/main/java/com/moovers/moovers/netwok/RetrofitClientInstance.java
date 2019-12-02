fa04046452172080521baca0ef1b8d4e1695e4bd
enyason <enyasonjnr@gmail.com>
2019-12-02 11:46:19 +0100

    implemented retrofit

---
+++ app/src/main/java/com/moovers/moovers/netwok/RetrofitClientInstance.java
@@ -0,0 +1,33 @@
+package com.moovers.moovers.netwok;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClientInstance {
+
+    private static Retrofit retrofit;
+
+    private static final String BASE_URL = "https://sheetsu.com/apis/v1.0su/e5cc1630393f";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+
+            Retrofit.Builder builder =
+                    new Retrofit.Builder()
+                            .baseUrl(BASE_URL)
+                            .addConverterFactory(
+                                    GsonConverterFactory.create()
+                            );
+
+            retrofit =
+                    builder
+                            .client(
+                                    httpClient.build()
+                            )
+                            .build();
+        }
+        return retrofit;
+    }
+}
